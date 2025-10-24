eb30cca842398276bf3d18bf4d5473594ada7b6f
Andrei Kostygov <kosandr17@mail.ru>
2025-10-24 20:24:24 +0300

    lab-5-3: try 275

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -33,7 +33,7 @@ class APIController(prometheusRegistry: MeterRegistry) {
     private val leakingBucketRateLimiter = LeakingBucketRateLimiter(
         11,
         Duration.ofSeconds(1),
-        270
+        275
     )
 
     @PostMapping("/users")
