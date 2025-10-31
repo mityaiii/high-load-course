9d43869c824c8d4f8759732dae6a7b9eb3e0a0cd
Andrei Kostygov <kosandr17@mail.ru>
2025-10-31 17:27:37 +0300

    lab-5-2: test 320

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -36,7 +36,7 @@ class APIController(prometheusRegistry: MeterRegistry) {
     private val leakingBucketRateLimiter = LeakingBucketRateLimiter(
         rateLimitPerSec.toLong(),
         Duration.ofSeconds(1),
-        409
+        320
     )
 
     @PostMapping("/users")
--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -30,7 +30,7 @@ class OrderPayer {
         16,
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(409),
+        LinkedBlockingQueue(320),
         NamedThreadFactory("payment-submission-executor"),
         ThreadPoolExecutor.AbortPolicy()
     )
